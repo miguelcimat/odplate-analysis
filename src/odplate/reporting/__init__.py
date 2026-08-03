c5f7190e825d1b40e84c97499866054d3b2a9a70
Miguel Ángel Álvarez Carmona <139270551+miguelcimat@users.noreply.github.com>
2026-08-03 13:07:55 -0600

    Update __init__.py

--- src/odplate/reporting/__init__.py
+++ src/odplate/reporting/__init__.py
@@ -1,1 +1,9 @@
-from .core import *
+from .reporting import (
+    exportar_excel,
+    exportar_csv,
+    generar_html,
+    generar_docx,
+    generar_pdf_desde_docx,
+    exportar_matrices_por_id,
+    exportar_series_por_grupo,
+)
